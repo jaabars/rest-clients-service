b09a0d57be4d692a20370c9d199734ad731f180f
aziret <aziret@mancho.dev>
2022-09-01 19:23:19 +0600

    added products models  class

---
+++ src/com/company/ClientProduct.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class ClientProduct extends Product {
+  private int quantity;
+  private double amount;
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  public double getAmount() {
+    return amount;
+  }
+
+  public void setAmount(double amount) {
+    this.amount = amount;
+  }
+}
